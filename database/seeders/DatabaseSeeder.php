4f235bc0a1d351c1348c075dda7c8c98b665973d
monciego <jerichobantiquete@gmail.com>
2025-01-16 04:15:08 +0800

    feat: change name

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Superadministrator',
+            'name' => 'Supreme Student Council',
             'email' => 'superadministrator@psu.com',
         ]);
 
